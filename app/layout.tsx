457a88424f30211d15552e17b1418052b527db8a
Ramit Prayas <mail.ramit2499@gmail.com>
2026-01-21 22:09:03 +0530

    updated layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,11 @@
 import "./globals.css";
 import Sidebar from "@/components/side-bar";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Infa Email",
+  description: "Choose Infrastructure Where You Want to Host Your Email",
+};
 
 export default function RootLayout({
   children,
